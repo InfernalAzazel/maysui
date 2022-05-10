7ac7a89a88d9e225aa4a8003e151bb5011669feb
Zhang Changhui <changhui.zhang.ext@desay-svautomotive.com>
2022-05-10 15:49:39 +0800

    更新代码

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'packages/index.ts'),
       name: 'MaysUI',
+      fileName: (format) => `maysui.${format}.ts`
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
